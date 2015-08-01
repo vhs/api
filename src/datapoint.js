75db5fb914802f41011174a245a1b7c2e2640317
James Sapara <james.sapara@gmail.com>
2015-08-01 13:10:08 -0700

    mocked datapoint access

--- src/datapoint.js
+++ src/datapoint.js
@@ -2,17 +2,17 @@
 var Datapoint = {
   // Get a datapoint
   get: function(redis, space, name, cb) {
-
+    cb(null, {value:"nope", name:"coffee", last_updated: 0});
   },
 
   // Set a datapoint
   set: function(redis, space, name, value, cb) {
-
+    cb(null);
   },
 
   // get the last 100 data points
   history: function(redis, space, name, cb) {
-
+    cb(null, []);
   }
 }
 
