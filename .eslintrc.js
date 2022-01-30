10f190c5e631a6b2765bab04a4d1ce1c4e9e2f9a
Ty Eggen <tycho.eggen@gmail.com>
2022-01-30 14:03:15 -0800

    eslint formatting/enforcement

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'xo',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    indent: ['error', 2],
+    'space-in-parens': ['error', 'never'],
+    'object-curly-spacing': ['error', 'always'],
+    camelcase: ['error', { properties: 'never', allow: ['request_hash', 'checked_hash', /influx_.*/] }],
+    radix: ['error', 'as-needed'],
+    'no-multiple-empty-lines': 'error',
+    'max-params': ['error', { max: 5 }],
+  },
+};
